6d1a0c4234c42a7c5900b7b0022253b43944b86c
Benoît Hubert <benoithubert@gmail.com>
2022-03-13 18:47:22 +0100

    setup Babel/JSX

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,26 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    reactApp: './src/reactApp/index.js',
+    reactApp: './src/reactApp/index.jsx',
     courseApp: './src/courseApp/index.js',
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          // options: {
+          //   presets: ['@babel/preset-env', '@babel/preset-react'],
+          // },
+        },
+      },
+    ],
   },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
 };
