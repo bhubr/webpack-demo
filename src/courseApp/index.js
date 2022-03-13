71d0971d71fcc838237a877352a75b0076b9b194
Benoît Hubert <benoithubert@gmail.com>
2022-03-13 21:02:40 +0100

    Install Showdownjs for course

--- src/courseApp/index.js
+++ src/courseApp/index.js
@@ -1,8 +1,16 @@
+import { Converter } from 'showdown';
+
+const converter = new Converter();
+
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'markdown'], ' ');
+  element.innerHTML = converter.makeHtml(`
+# Hello Markdown
+
+This is some text. This is [a link to React docs](https://reactjs.org).
+  `)
 
   return element;
 }
